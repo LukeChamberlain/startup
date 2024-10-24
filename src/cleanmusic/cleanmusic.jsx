fbed549923a4eb24f09ee22c7dd420beb778406d
LukeChamberlain <LukeChamberlain64@gmail.com>
2024-10-24 15:10:22 -0600

    Created all the jsx files

---
+++ src/cleanmusic/cleanmusic.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Cleanmusic() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>clean music displayed here</div>
+    </main>
+  );
+}
