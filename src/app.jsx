93275e2eb3975a1ff8a00ce1bb48d43f4534164f
LukeChamberlain <LukeChamberlain64@gmail.com>
2024-10-30 15:28:57 -0600

    created stubs for all jsx files and put them in corresponding folders

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,34 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className='body bg-light text-dark'>
+      <header className='container-fluid'>
+        <nav className='navbar fixed-top navbar-light'>
+          <menu class="navbar-nav ms-auto d-flex justify-content-start gap-3">
+            <a class="nav-link active" href="index.html">Home</a>
+            <li class="nav-item">
+              <a class="nav-link" href="profile.html">Your Music</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="cleanmusic.html">Clean Your Music</a>
+            </li>
+          </menu>
+        </nav>
+      </header>
+
+      <main>App components go here</main>
+
+      <footer className='bg-light text-dark'>
+        <div className='container-fluid'>
+          <span className='text-reset'>Luke Chamberlain</span>
+          <a className='text-reset' href="https://github.com/LukeChamberlain/startup">
+            Github
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
---
+++ src/database/database.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function database() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>login displayed here</div>
+    </main>
+  );
+}
