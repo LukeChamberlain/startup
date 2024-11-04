e91be66aec3e9f842046ffe21ee3ab85d60ca6ed
LukeChamberlain <LukeChamberlain64@gmail.com>
2024-11-04 10:43:38 -0700

    added authentication to the login page

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-
 import Login from './login/Login';
 import CleanMusic from './cleanmusic/CleanMusic';
 import Profile from './profile/Profile';
 import Filters from './filters/Filters';
 import Database from './database/Database';
 
+const AuthState = {
+  Authenticated: 'authenticated',
+  Unauthenticated: 'unauthenticated'
+};
+
 export default function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+
   return (
     <BrowserRouter>
       <div className='body bg-light text-dark'>
@@ -27,16 +35,26 @@ export default function App() {
           </nav>
         </header>
 
-        <main>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/cleanmusic" element={<CleanMusic />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/filters" element={<Filters />} />
-            <Route path="/database" element={<Database />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(newUserName, newAuthState) => {
+                  setAuthState(newAuthState);
+                  setUserName(newUserName);
+                }}
+              />
+            }
+          />
+          <Route path="/cleanmusic" element={<CleanMusic />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/filters" element={<Filters />} />
+          <Route path="/database" element={<Database />} />
+        </Routes>
 
         <footer className='bg-light text-dark'>
           <div className='container-fluid'>
